0b3846e2a0b9b4971b5b23a6f9fcfb21a7bb352a
Patrick Baldwin <pat@relentlesstechnology.com>
2018-02-09 08:44:57 -0800

    added places search to replace city/postal and locality inputs removed search button in favour of auto-updating on change

--- scripts/find-an-alpha.js
+++ scripts/find-an-alpha.js
@@ -7,7 +7,7 @@ FindAnAlpha = Polymer({
 	properties: {
 		apiKey: {
 			type: String,
-			value : 'AIzaSyCliv-zsZo4mT5elv3DNAnSmjO404plV50'
+			value: 'AIzaSyCliv-zsZo4mT5elv3DNAnSmjO404plV50'
 		},
 		latitude: {
 			type: Number,
@@ -49,22 +49,6 @@ FindAnAlpha = Polymer({
 			type: Object,
 			value: null
 		},
-		submitting: {
-			type: Boolean,
-			value: false
-		},
-		localities: {
-			type: Array,
-			value: null
-		},
-		localitiesLabel: {
-			type: String,
-			value: "Province"
-		},
-		postalZipLabel: {
-			type: String,
-			value: "Postal Code"
-		},
 		agesLabel: {
 			type: String,
 			value: "Audience"
@@ -77,12 +61,18 @@ FindAnAlpha = Polymer({
 			type: Array,
 			value: null
 		},
-		radiuses: {
-			type: Array,
-			value: null
+		radius: {
+			type: String,
+			value: '100km'
 		},
-		city: String,
-		postalFormatRegex: String
+		placesOptions: {
+			type: Object,
+			value: {}
+		},
+		isLoading: {
+			type: Boolean,
+			value: false
+		}
 
 	},
 
@@ -91,100 +81,69 @@ FindAnAlpha = Polymer({
 	},
 
 	ready: function () {
-
-		var self = this;
-
-		var provField = this.querySelector('#province');
-		if (provField) {
-			provField.onchange = function () {
-				self.$.errorMessage.innerHTML = "";
-			};
-		}
+		var mql = window.matchMedia('(max-width: 767px)');
+		mql.addListener(this.onMediaQuery.bind(this));
 
 	},
-	attached: function () {
-		var provField = this.querySelector('#province');
+	attached: function () { },
+	_mapsReady: function () {
 
-		if (this.city || (provField && provField.value)) {
-			this.submitForm();
-		}
+		this.autoComplete = new google.maps.places.Autocomplete(this.$.placesSearch, this.placesOptions);
+
+		this.autoComplete.bindTo('bounds', this.map);
+
+		this.autoComplete.addListener('place_changed', this._onPlaceSelected.bind(this));
+	},
+
+	_onPlaceSelected: function () {
+
+		var place = this.autoComplete.getPlace();
+
+		this.search_latitude = place.geometry.location.lat();
+		this.search_longitude = place.geometry.location.lng();
+
+		this.submitForm();
 
 	},
 
 	shareLocation: function () {
-		var self = this;
-		this.submitting = true;
-		var geo = this.$.geo = document.createElement("geo-location");
-		geo.latitude = this.latitude;
-		geo.longitude = this.longitude;
-
-		geo.addEventListener("latitude-changed", function (ev) {
-			self.latitude = this.latitude;
-		});
-
-		geo.addEventListener("longitude-changed", function (ev) {
-			self.longitude = this.longitude;
-		});
-
-		this.$.search.appendChild(geo);
-
-		self.geoHandler = function (e) {
-			var request = this.querySelector('#geo-search')
-			var form = this.querySelector('form');
-			var data = form.serialize();
-			delete data.cityorpostalcode;
-			delete data.province;
-			data.latitude = e.detail.latitude;
-			data.longitude = e.detail.longitude;
-			request.params = data;
-			request.lastResponse = this.resultList;
-			request.addEventListener('response', self.onResult.bind(self));
-			request.generateRequest();
-			self.foundGeo = true;
-		};
-
-		this.$.search.addEventListener('geo-response', self.geoHandler);
+		this.$.placesSearch.value = '';
+		this.$.geoLocation.idle = false;
+		this.$.geoLocation.fetch();
 
 	},
-	submitForm: function () {
-		var self = this;
-		var form = this.querySelector('form');
-		var cityorpostalcode = this.$.cityorpostalcode.value;
-		var hasCityorpostalcode = cityorpostalcode !== "";
-		var isPostalCode = this._isPostalCode(cityorpostalcode);
-		var isTryingPostal = new RegExp(/\d/).test(cityorpostalcode);
-		var hasProvince = this.querySelector('#province').value !== "";
+	onGeoLocate: function (event) {
+		this.search_latitude = event.detail.latitude;
+		this.search_longitude = event.detail.longitude;
 
-		if (isTryingPostal && !isPostalCode) {
-			this.$.errorMessage.innerHTML = "Please enter a valid " + this.postalZipLabel + "";
-			return;
-		} else if (!isPostalCode && !hasProvince && hasCityorpostalcode) {
-			this.$.errorMessage.innerHTML = "Please enter a " + this.localitiesLabel + " to continue.";
+		this.submitForm();
+	},
+	onGeoError: function (event) {
+		if (event.detail.code === 1) {
+			// user said no
+			this.$.errorMessage.innerHTML = "We can't find Alphas near you without your location. You can enable browser location by clicking on the info icon to left of your address bar.";
+		}
+		console.info(event.detail);
+	},
+	submitForm: function () {
+
+		if (!this.search_latitude) {
 			return;
 		}
-		if (isPostalCode) {
-			this.$.cityorpostalcode.name = "postalCode";
-		} else {
-			this.$.cityorpostalcode.name = "city";
-		}
-		form.submit();
-		form.addEventListener('iron-form-response', self.onResult.bind(self));
-		form.addEventListener('iron-form-error', self._onError.bind(self));
-		self.foundGeo = false;
+
+		this.isLoading = true;
+		this.$.searchForm.submit();
+
 	},
 
 	onResult: function (event) {
-		this.submitting = false;
-		if (this.$.geo) {
-			this.$.geo.remove();
-		}
-		this.$.search.removeEventListener('geo-response', this.geoHandler);
+		this.isLoading = false;
+		this.loadedOnce = true;
+
 		this.count = event.detail.response.count;
 		this.measurementUnits = event.detail.response.radius.units;
 
-		if (this.foundGeo) {
-			this._getAddress(event.detail.response.locations[0]);
-		}
+		this.$.markerClusterer.markers = [];
 
 		if (this.count === 0) {
 
@@ -192,57 +151,32 @@ FindAnAlpha = Polymer({
 			return;
 		}
 
-		var pages = this.querySelector('iron-pages');
-		pages.select(1);
 		this.resultList = event.detail.response.items;
 
-		var map = this.querySelector('google-map');
-		var selector = this.querySelector('google-map iron-selector');
-		var markers = this.querySelector('google-map-marker');
-		if (markers) {
-			selector.removeChild(markers);
-		}
+		this.$.ironPages.select(1);
 
-		map.removeAttribute('zoom');
-		map.setAttribute('fit-to-markers', true);
+		this.$.googleMap.removeAttribute('zoom');
+		this.$.googleMap.setAttribute('fit-to-markers', true);
 		this._setAllMarkers();
 	},
 
-	onCityKeyUp: function (event, detail) {
-
-		var value = this.$.cityorpostalcode.value;
-
-		var isPostalCode = this._isPostalCode(value);
-		var isTryingPostal = new RegExp(/\d/).test(value);
-
-		var message = '';
-		if (isTryingPostal && !isPostalCode) {
-			message = "Please enter a valid " + this.postalZipLabel + "";
-
-		}
-		this.$.errorMessage.innerHTML = message;
-	},
-
-
 	backToSearch: function () {
-		var resultPage = this.querySelector('neon-animated-pages');
-		resultPage.select(0);
-		var pages = this.querySelector('iron-pages');
-		pages.select(0);
+
+		// reset list view
+		this.$.list.select(0);
+		// go to search form
+		this.$.ironPages.select(0);
+		// clear message
 		this.$.errorMessage.innerHTML = "";
-		this._setDefaultMarker();
-		resultPage = this.querySelector('neon-animated-pages').select(0);
-		if (this.foundGeo && this.city) {
-			this.$.cityorpostalcode.value = this.city;
-		}
+
 	},
 
 	_onMarkerClick: function (marker) {
 		var mark = marker.label;
 		var index = Number(mark - 1);
 		this.selected = this.resultList[index];
-		var pages = this.querySelector('iron-pages');
-		pages.select(1);
+
+		this.$.ironPages.select(1);
 		this.$.list.selected = 1;
 		this._markerSelected(index);
 
@@ -265,13 +199,13 @@ FindAnAlpha = Polymer({
 	},
 
 	_markerSelected: function (index) {
-		var self = this;
-		var map = this.querySelector('google-map');
-		var cluster = this.querySelector('google-map-markerclusterer');
+
+		var map = this.$.googleMap;
+		var cluster = this.$.markerClusterer;
 		cluster.markers = [];
 
 		var mark = Number(index) + 1;
-		var element = self.resultList[index];
+		var element = this.resultList[index];
 		var myLatLng = new google.maps.LatLng(element.lat, element.lng)
 		var newmarker = new google.maps.Marker({
 			position: myLatLng,
@@ -309,7 +243,7 @@ FindAnAlpha = Polymer({
 			markers[index] = newmarker;
 			bounds.extend(newmarker.getPosition());
 		});
-		var gmap = this.querySelector('google-map');
+		var gmap = this.$.googleMap;
 		gmap.map.fitBounds(bounds);
 
 		var curZoom = gmap.map.getZoom();
@@ -317,71 +251,62 @@ FindAnAlpha = Polymer({
 			gmap.map.setZoom(this.maxZoom);
 		}
 
-		this.querySelector('google-map-markerclusterer').map = gmap.map;
-		this.querySelector('google-map-markerclusterer').markers = markers;
-	},
-
-	_setDefaultMarker: function () {
-		var self = this;
-		var map = this.$$('google-map');
-
-		if (self.foundGeo === true) {
-			self._setAllMarkers();
-
-			return;
-		}
-		self.querySelector('google-map-markerclusterer').markers = [];
-		map.setAttribute('latitude', this.latitude);
-		map.setAttribute('longitude', this.longitude);
-		map.setAttribute('zoom', this.zoom);
-		map.removeAttribute('fit-to-markers');
+		this.$.markerClusterer.map = gmap.map;
+		this.$.markerClusterer.markers = markers;
 	},
 
 
+	onMediaQuery: function (event) {
+		this._adjustHeight();
+	},
 
-	_adjustHeight: function (event) {
+	_adjustHeight: function () {
+
+		var el = this.$.listView,// event.target,
+			polyEl = Polymer.dom(el);
 
 		if (screen.width < 768) {
-			var el = event.target;//this.$$('list-view')
+
 			var biggestHeight = 0;
 			// Loop through elements children to find & set the biggest height
 
-			Polymer.dom(el).node.childNodes.forEach(function (element) {
+			var navBar = this.querySelector('#nav-bar'),
+				alerts = this.querySelector('#alerts'),
+				listItems = el.querySelectorAll('#location');
 
-				biggestHeight = biggestHeight + element.offsetHeight;
+			biggestHeight += navBar.offsetHeight;
+			biggestHeight += alerts ? alerts.offsetHeight : 0;
 
-			});
+			if (this.$.list.selected === 1) {
+
+				biggestHeight += 300;
+
+			} else {
+
+				if (listItems.length) {
+					biggestHeight += 12;
+				}
+
+				listItems.forEach(function (element) {
+
+					biggestHeight = biggestHeight + element.offsetHeight;
+
+				});
+			}
+
+			var height = biggestHeight > 300 ? 300 : biggestHeight;
 
 			// Set the container height
-			Polymer.dom(el).parentNode.style.height = biggestHeight + "px";
+			polyEl.parentNode.style.height = height + "px";
+		} else {
+			polyEl.parentNode.style.height = '';
 		}
-	},
-
-	_isPostalCode: function (postal) {
-
-		if (this.postalFormatRegex) {
-			var rgx = eval(this.postalFormatRegex);//
-			return rgx.test(postal);
-		}
-		return false;
 	},
 
 	_onError: function () {
+		this.isLoading = false;
 		this.$.errorMessage.innerHTML = "Hmm... we can't find that location. Please try again.";
 	},
-	_getAddress: function (results) {
-		var self = this;
-		for (var i = 0; i < results.address_components.length; i++) {
-			if (results.address_components[i].types[0] == "locality") {
-				//this is the object you are looking for
-				self.city = results.address_components[i].long_name;
-			}
-			if (results.address_components[i].types[0] == "administrative_area_level_1") {
-				//this is the object you are looking for
-				self.province = results.address_components[i].long_name;
-			}
-		}
-	}
 
 });
 
